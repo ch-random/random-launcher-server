c6878a8a71275af0625c9842df2f20c1e4220d7b
RasPython3 <87003300+RasPython3@users.noreply.github.com>
2025-10-25 21:26:39 +0900

    Implement fastapi

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,8 +3,11 @@ from typing import Optional
 import multiprocessing
 
 import fastapi
-from fastapi import APIRouter, Body, Depends, Path, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import FileResponse, StreamingResponse
 import datetime
+
+import zipfile
 
 from ..abc import *
 from ..content import ContentList, ContentManager
@@ -19,37 +22,79 @@ api = APIRouter()
 content_manager = ContentManager()
 
 @api.get("/contents")
-async def get_contents(platform: Optional[Platform], contents: ContentList = Depends(content_manager.on_fastapi_depends)):
+async def get_contents(platform: Optional[Platform] = None, contents: ContentList = Depends(content_manager.on_fastapi_depends)):
     """
     (platformが指定されればそのプラットフォームに対応する)すべてのコンテンツのメタデータを返す
     """
     with contents.use() as c:
-        print([*c])
+        return [csrc.content for csrc in c if platform is None or platform in csrc.content.supported_platforms]
 
 @api.get("/content/{content_id}")
-async def get_content_meta(content_id: str):
+async def get_content_meta(content_id: str, contents: ContentList = Depends(content_manager.on_fastapi_depends)):
     """
     id == content_id であるコンテンツのメタデータで応答する
     """
-    pass
+    with contents.use() as c:
+        for csrc in c:
+            if csrc.content.id == content_id:
+                return csrc.content
+    raise HTTPException(status_code=404, detail="content not found")
 
-@api.get("/content/{content_id}")
-async def get_content_zip(content_id: str):
+@api.get("/content/{content_id}/zip", response_class=FileResponse)
+async def get_content_zip(content_id: str, contents: ContentList = Depends(content_manager.on_fastapi_depends)):
     """
-    id == content_id であるコンテンツのメタデータで応答する
+    id == content_id であるコンテンツのzipで応答する
     """
-    pass
+    src = None
+    with contents.use() as c:
+        for csrc in c:
+            if csrc.content.id == content_id:
+                src = csrc
+                break
 
-@api.get("/content/{content_id}/thumbnail")
-async def get_content_zip(content_id: str):
+        if src is None:
+            raise HTTPException(status_code=404, detail="content not found")
+
+    return FileResponse(src.path, media_type="application/zip")
+
+@api.get("/content/{content_id}/thumbnail", response_class=StreamingResponse)
+async def get_content_thumbnail(content_id: str, contents: ContentList = Depends(content_manager.on_fastapi_depends)):
     """
     id == content_id であるコンテンツのサムネイル画像で応答する
     """
-    pass
+    src = None
+    with contents.use() as c:
+        for csrc in c:
+            if csrc.content.id == content_id:
+                src = csrc
+                break
+
+        if src is None:
+            raise HTTPException(status_code=404, detail="content not found")
+
+        if src.content.thumbnail is None:
+            raise HTTPException(status_code=404, detail="no thumbnail")
+
+        with zipfile.ZipFile(src.path) as zf:
+            with zf.open(src.content.thumbnail) as f:
+                data = f.read()
+
+    async def iterdata():
+        for b in data:
+            yield b
+
+    return StreamingResponse(iterdata(), media_type="image/*")
 
 @api.get("/updates")
-async def updates(since: datetime.datetime):
+async def updates(since: datetime.datetime, platform: Optional[Platform] = None, contents: ContentList = Depends(content_manager.on_fastapi_depends)):
     """
     since以降に更新/削除されたコンテンツを返す
     """
-    pass
+    since = since.replace(tzinfo=datetime.timezone.utc)
+    with contents.use() as c:
+        return [
+            csrc.content for csrc in c
+            if platform is None or platform in csrc.content.supported_platforms
+            if csrc.content.last_modified >= since
+        ]
+
