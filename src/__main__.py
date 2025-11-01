1ee4fe5b54c6d5be90d10cadeec4430aa650e425
らすぴー <87003300+RasPython3@users.noreply.github.com>
2025-11-01 21:11:53 +0900

    Use settings

--- src/__main__.py
+++ src/__main__.py
@@ -1,19 +1,9 @@
-import argparse
 import uvicorn
 import multiprocessing
 
-from . import app
+from . import app, settings
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--host")
-    parser.add_argument("target_dir") 
-
-    args = parser.parse_args()
-
-    host = args.host if args.host else "127.0.0.1"
-
-    uvicorn.run(app, host=host, port=8080)
+    uvicorn.run(app, host=settings.API_HOST, port=settings.API_PORT)
